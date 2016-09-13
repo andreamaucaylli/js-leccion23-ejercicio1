ced272ae594a3ee389bde09bbd8c1ac1b9a1d28f
Andrea Maucaylli C <Andrea Maucaylli C>
2016-09-13 16:07:54 -0500

    html, js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+ window.addEventListener("load", function() { 
+   var boton = document.getElementById("calcular"); 
+   boton.addEventListener("click", function() { 
+       var nombre= document.getElementById("nombre").value; 
+	   var edad= parseInt(document.getElementById("edad").value); 
+ 	   var sexo= document.getElementById("sexo").value; 
+ 
+ 		function Persona(nombre,edad,sexo){ 
+ 			this.nombre=nombre; 
+ 			this.edad = edad; 
+ 			this.sexo= sexo; 
+ 			this.mayorDeEdad =(this.edad >=18); 
+ 			this.presentacion = function() 
+ 			{ 
+ 				if (this.mayorDeEdad){ 
+ 					return "Hola, mi nombre es " + this.nombre +  ", tengo "+ this.edad  + " años y soy mayor de edad."; 
+ 				}else{ 
+ 					return "Hola, mi nombre es " + this.nombre +  ", tengo "+ this.edad  + " años y soy menor de edad."; 
+ 				} 
+ 			} 
+ 		} 
+ 
+ 
+ 			var persona1 = new Persona(nombre,edad,sexo); 
+ 				document.write(persona1.presentacion());             
+	}); 
+}); 
